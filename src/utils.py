745c2de761e2819c69acf2abb73cc46701e50810
Maria Fedorova <maria.i.fedorova@gmail.com>
2024-02-21 22:07:23 +0300

    additional task_new version

--- src/utils.py
+++ src/utils.py
@@ -18,11 +18,12 @@ def create_objects_category(data):
         if len(data) != 0:
             category = classes.Category(object['name'], object['description'], object['products'])
             new_category.append(category)
-            #product = classes.Product(object['products']['name'], object['products']['description'], object['products']['price'], object['products']['quantity'])
-            #new_product.append(product)
+            for i_product in object['products']:
+                product = classes.Product(i_product['name'], i_product['description'], i_product['price'], i_product['quantity'])
+                new_product.append(product)
     print(new_category)
     print(new_product)
-    return new_category, #new_product
+    return new_category, new_product
 #print(create_objects_category(load_data('products.json')))
 #print(create_objects_category(load_data('products.json'))[1])
 
