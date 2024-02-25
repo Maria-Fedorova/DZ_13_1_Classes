89ff96b4c7fe255b14e5bd54afa8233784676ee5
Maria Fedorova <maria.i.fedorova@yandex.ru>
2024-02-25 17:16:58 +0300

    доработка домашки

--- src/classes.py
+++ src/classes.py
@@ -1,6 +1,6 @@
 class Category:
 
-    '''Создание класса Category'''
+    """Создание класса Category"""
 
     # Свойства (атрибуты) класса
     name: str
@@ -9,23 +9,24 @@ class Category:
 
     # Переменные на уровне класса
     number_of_categories = 0
-
+    number_of_unique_products = 0
 
     def __init__(self, name, description, products):
-        '''Конструктор для инициализации экземпляра класса.
-        Задаем значения атрибутам экземпляра класса'''
+        """Конструктор для инициализации экземпляра класса.
+        Задаем значения атрибутам экземпляра класса"""
         self.name = name
         self.description = description
         self.products = products
-
-        Category.number_of_categories += 1
+        self.number_of_unique_products += len(self.products)
+        self.number_of_categories += 1
 
     def __repr__(self):
         return f'name={self.name}, description={self.description}, products={self.products})'
 
+
 class Product:
 
-    '''Создание класса Product'''
+    """Создание класса Product"""
 
     # Свойства (атрибуты) класса
     name: str
@@ -33,19 +34,13 @@ class Product:
     price: float
     quantity: int
 
-    # Переменные на уровне класса
-    number_of_unique_products = 0
-
-
     def __init__(self, name, description, price, quantity):
-        '''Конструктор для инициализации экземпляра класса.
-            Задаем значения атрибутам экземпляра класса'''
+        """Конструктор для инициализации экземпляра класса.
+            Задаем значения атрибутам экземпляра класса"""
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
-
-        Product.number_of_unique_products += 1
 
     def __repr__(self):
         return f'name={self.name}, description={self.description}, price={self.price}, number_present={self.quantity})'
