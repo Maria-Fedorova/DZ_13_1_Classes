b971c0157dca67d565e2a153269535abbbce932c
Maria Fedorova <maria.i.fedorova@yandex.ru>
2024-03-11 00:53:44 +0300

    classes and test_classes modified to accomplish the task 13.2 (ACCESS_LESSON)

--- src/classes.py
+++ src/classes.py
@@ -16,12 +16,21 @@ class Category:
         Задаем значения атрибутам экземпляра класса"""
         self.name = name
         self.description = description
-        self.products = products
-        self.number_of_unique_products += len(self.products)
+        self.__products = products
+        self.number_of_unique_products += len(self.__products)
         self.number_of_categories += 1
 
+    @property
+    def products(self):
+        """Геттер, который выводит список товаров в формате:
+        Продукт, 80 руб. Остаток: 15 шт."""
+        list_product = []
+        for product in self.__products:
+            list_product.append(f'{product["name"]}, {product["price"]} руб. Остаток: {product["quantity"]} шт.')
+        return "\n".join(list_product)
+
     def __repr__(self):
-        return f'name={self.name}, description={self.description}, products={self.products})'
+        return f'name={self.name}, description={self.description}, products={self.__products})'
 
 
 class Product:
@@ -41,6 +50,27 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def create_product(cls, name, description, price, quantity):
+        """
+        Создает товар и возвращает объект, который можно добавлять в список товаров
+        """
+        return {"name": name, "description": description, "price": price, "quantity": quantity}
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена введена некорректная")
+        self.__price = new_price
+
+    @price.deleter
+    def price(self):
+        self.__price = None
 
     def __repr__(self):
         return f'name={self.name}, description={self.description}, price={self.price}, number_present={self.quantity})'
